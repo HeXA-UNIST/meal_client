95868e49f846be5c2f52dfd6679a20bb2ee7b2b1
stackbof <163146660+stackbof@users.noreply.github.com>
2025-08-05 00:00:00 +0000

    add storage management functions

---
+++ lib/storage.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+
+Future<void> saveFileAsString(String fileName, String data) async {
+  final dir = await getApplicationSupportDirectory();
+  final file = File("${dir.path}/$fileName");
+  await file.writeAsString(data, flush: true);
+}
+
+Future<String> readFileAsString(String fileName) async {
+  final dir = await getApplicationSupportDirectory();
+  final file = File("${dir.path}/$fileName");
+  return await file.readAsString();
+}
