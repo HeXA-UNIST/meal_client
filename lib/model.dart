1b1fe2cb9191560eaa702c8e0a1f336c755ead71
stackbof <163146660+stackbof@users.noreply.github.com>
2025-08-08 00:00:00 +0000

    make variables of model private and add some methods

--- lib/model.dart
+++ lib/model.dart
@@ -4,23 +4,60 @@ import 'i18n.dart';
 import 'meal.dart';
 
 class BapUModel extends ChangeNotifier {
-  Language language;
-  Brightness brightness;
-
-  int month;
-  int day;
-  MealOfDay mealOfDay;
+  Language _language;
+  Brightness _brightness;
+  int _month;
+  int _day;
+  MealOfDay _mealOfDay;
 
   BapUModel({
-    required this.language,
-    required this.brightness,
-    required this.month,
-    required this.day,
-    required this.mealOfDay,
-  });
+    required Language language,
+    required Brightness brightness,
+    required int month,
+    required int day,
+    required MealOfDay mealOfDay,
+  }) : _language = language,
+       _brightness = brightness,
+       _month = month,
+       _day = day,
+       _mealOfDay = mealOfDay;
+
+  Language get language => _language;
+
+  Brightness get brightness => _brightness;
+
+  int get month => _month;
+
+  int get day => _day;
+
+  MealOfDay get mealOfDay => _mealOfDay;
+
+  void changeLanguage(Language language) {
+    _language = language;
+
+    notifyListeners();
+  }
+
+  void toggleBrightness() {
+    if (_brightness == Brightness.light) {
+      _brightness = Brightness.dark;
+    } else {
+      _brightness = Brightness.light;
+    }
+
+    notifyListeners();
+  }
+
+  void changeDate(int month, int day) {
+    _month = month;
+    _day = day;
+
+    notifyListeners();
+  }
 
   void switchToNextMealOfDay() {
-    mealOfDay = nextMealOfDay(mealOfDay);
+    _mealOfDay = nextMealOfDay(_mealOfDay);
+
     notifyListeners();
   }
 }
