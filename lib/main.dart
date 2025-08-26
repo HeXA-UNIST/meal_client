02a473c68609007fc4634475414173f0cf6e59f1
stackbof <163146660+stackbof@users.noreply.github.com>
2025-08-26 00:00:00 +0000

    rollback custom font fix as it breaks dark theme text color

--- lib/main.dart
+++ lib/main.dart
@@ -63,9 +63,6 @@ class MyApp extends StatelessWidget {
           title: string.title.getLocalizedString(bapu.language),
           theme: ThemeData(
             fontFamily: 'Pretendard',
-            textTheme: Theme.of(
-              context,
-            ).textTheme.apply(fontFamily: "Pretendard"),
             brightness: bapu.brightness,
             colorScheme:
                 ColorScheme.fromSeed(
