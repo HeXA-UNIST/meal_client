d29aaee3c075e63b26a714690cde5a6cf9b31122
downtown1629 <165543046+downtown1629@users.noreply.github.com>
2026-06-22 12:48:50 +0900

    chore: package upgrade and setup - Minimum IOS version is now 15 (cupertino_http)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,6 +13,7 @@ android {
     ndkVersion = "28.2.13676358"
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -43,4 +44,10 @@ android {
 
 flutter {
     source = "../.."
+}
+
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+    implementation("androidx.work:work-runtime-ktx:2.10.1")
 }
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import workmanager_apple
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -7,6 +8,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
@@ -14,3 +16,7 @@ import UIKit
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
 }
+WorkmanagerPlugin.registerPeriodicTask(
+  withIdentifier: "com.bapu.periodic_task",
+  frequency: NSNumber(value: 15 * 60) // 20 minutes (15 min minimum)
+)
