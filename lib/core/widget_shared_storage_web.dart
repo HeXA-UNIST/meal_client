bf91930c7a9f2fb2b581bb2ccbe39c8e5493a5bd
downtown1629 <165543046+downtown1629@users.noreply.github.com>
2026-07-05 23:01:32 +0900

    web 위젯 미지원 예외처리 단순화

--- lib/core/widget_shared_storage_web.dart
+++ lib/core/widget_shared_storage_web.dart
@@ -1,13 +1,12 @@
-Future<Never> sharedWidgetCacheDir() async {
-  throw UnsupportedError('웹 플랫폼은 위젯 공유 캐시를 지원하지 않습니다');
-}
+Never _unsupportedOnWeb() =>
+    throw UnsupportedError('웹 플랫폼은 위젯 공유 캐시를 지원하지 않습니다');
+
+Future<Never> sharedWidgetCacheDir() async => _unsupportedOnWeb();
 
 Future<void> saveSharedWidgetFileAsString(String fileName, String data) async {}
 
-Future<String> readSharedWidgetFileAsString(String fileName) async {
-  throw UnsupportedError('웹 플랫폼은 위젯 공유 캐시를 지원하지 않습니다');
-}
+Future<String> readSharedWidgetFileAsString(String fileName) async =>
+    _unsupportedOnWeb();
 
-Future<DateTime> getLastModifiedOfSharedWidgetFile(String fileName) async {
-  throw UnsupportedError('웹 플랫폼은 위젯 공유 캐시를 지원하지 않습니다');
-}
+Future<DateTime> getLastModifiedOfSharedWidgetFile(String fileName) async =>
+    _unsupportedOnWeb();
