cf3356f1ac5f7c98ea35f01efb9f7dc48542bfc2
downtown1629 <165543046+downtown1629@users.noreply.github.com>
2026-08-03 11:38:06 +0900

    Revert "Bump gradle version to 8.14 (나중에 9.x로 올리는거 대비)"
    This reverts commit 9f5b895a2c58af1c226edcec1d7cbe76a533d311.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
@@ -9,7 +10,6 @@ android {
     compileSdk = flutter.compileSdkVersion
     // temporarily change ndk version to ensure 16kb page size support
     // ndkVersion = flutter.ndkVersion
-    // https://blog.naver.com/rlawnguq12/223428084454
     ndkVersion = "28.2.13676358"
 
     compileOptions {
@@ -18,12 +18,9 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlin {
-        compilerOptions {
-            jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
-        }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
-
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
