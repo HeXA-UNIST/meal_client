e8e6a6294b2a732f30f300a5407aa494b1a6311e
downtown1629 <165543046+downtown1629@users.noreply.github.com>
2026-09-25 13:25:06 +0900

    refactor: simplify conditional imports for notification platform detection

--- lib/features/notification/notification_platform.dart
+++ lib/features/notification/notification_platform.dart
@@ -1,3 +1,11 @@
-export 'notification_platform_stub.dart'
-    if (dart.library.io) 'notification_platform_io.dart';
-export 'notification_platform_type.dart';
+import 'notification_platform_stub.dart'
+    if (dart.library.io) 'dart:io'
+    as platform;
+
+enum MealNotificationPlatform { android, ios, unsupported }
+
+MealNotificationPlatform get mealNotificationPlatform {
+  if (platform.Platform.isIOS) return MealNotificationPlatform.ios;
+  if (platform.Platform.isAndroid) return MealNotificationPlatform.android;
+  return MealNotificationPlatform.unsupported;
+}
